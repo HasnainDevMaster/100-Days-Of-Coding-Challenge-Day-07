197c1d1e2054157814e1fa87857e3961db6c6247
Syed Hasnain Ali Shah <syedhasnainali927@gmail.com>
2024-08-04 11:41:29 +0500

    Update TaskC.ts

--- TaskC.ts
+++ TaskC.ts
@@ -1,6 +1,6 @@
 // 100 Days Of Coding Challenge!
 
-/** Question:
+/** Question 21:
  Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.*/
 
 //------------------------------------------------------------------------------------------
